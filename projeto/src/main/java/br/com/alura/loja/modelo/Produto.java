f4a758ea9c6c2743720ad678f2b8022d7ea5d07e
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-06-19 13:18:35 -0300

    Mais mapeamentos.

--- projeto/src/main/java/br/com/alura/loja/modelo/Produto.java
+++ projeto/src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -1,8 +1,11 @@
 package br.com.alura.loja.modelo;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -17,6 +20,16 @@ public class Produto {
 	private String nome;
 	private String descricao;
 	private BigDecimal preco;
+	private LocalDate data = LocalDate.now();
+	@Enumerated(EnumType.STRING)
+	private Categoria categoria;
+
+	public Produto(String nome, String descricao, BigDecimal preco, Categoria categoria) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco = preco;
+		this.categoria = categoria;
+	}
 
 	public Integer getId() {
 		return id;
@@ -48,6 +61,22 @@ public class Produto {
 
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
 	}
 
 }
