d1267b74da456cf900c7965122a447a07ef052ef
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-06-19 12:46:42 -0300

    Organizando o código.

--- projeto/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ projeto/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -3,10 +3,10 @@ package br.com.alura.loja.testes;
 import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
+import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
 	public static void main(String[] args) {
@@ -15,12 +15,11 @@ public class CadastroDeProduto {
 		celular.setDescricao("Muito legal");
 		celular.setPreco(new BigDecimal(800));
 		
-		EntityManagerFactory factory = Persistence
-				.createEntityManagerFactory("loja");
-		EntityManager em = factory.createEntityManager();
-
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao dao = new ProdutoDao(em);
+		
 		em.getTransaction().begin();
-		em.persist(celular);
+		dao.cadastrar(celular);
 		em.getTransaction().commit();
 		
 		em.close();
